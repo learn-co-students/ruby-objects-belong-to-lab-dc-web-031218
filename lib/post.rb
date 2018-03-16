7240dcd6223b710bd0bb3cb26039cf1789f04801
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-03-16 09:37:17 -0400

    lab completed

---
+++ lib/post.rb
@@ -0,0 +1,10 @@
+# Posts should have a title and belong to an author.
+# A post should be able to tell you the name of its author
+
+class Post
+  attr_accessor :title, :author
+
+  def initialize
+    @author = Author.new
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,11 @@
+# Artists should have a name.
+# Songs should have a title and belong to an artist.
+# A song should be able to tell you the name of its artist
+
+class Song
+  attr_accessor :title, :artist
+
+  def initialize
+    @artist = Artist.new
+  end
+end
